94526ce28366492e84576be43885d5ba3a397a3c
Deepak Kumar <2871622+dpkmcaanna@users.noreply.github.com>
2024-09-14 19:08:24 +0530

    Time: 27 ms (37.04%), Space: 45.3 MB (84.19%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,55 +1,38 @@
-	class Task implements Comparable<Task>{
-		int frq;
-		int nextExecutionTime;
-		
-		public Task(int frq, int nextExecutionTime) {
-			super();
-			this.frq = frq;
-			this.nextExecutionTime = nextExecutionTime;
-		}
-
-        @Override
-        public int compareTo(Task other) {
-            return other.frq - this.frq;
-        }
-	}
-
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
-    	
-    	for(int c : tasks) {
-    		freq.put(c, freq.getOrDefault(c, 0) + 1);
-    	}
 
-        PriorityQueue<Task> execTak = new PriorityQueue<>();
+        for (int c : tasks) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
 
-        freq.entrySet().stream().forEach(e -> execTak.offer(new Task(e.getValue(), 0)));
+        PriorityQueue<int[]> execTak = new PriorityQueue<>((a, b) -> b[0] - a[0]);
 
-        Queue<Task> waitingQ = new LinkedList<>();
+        freq.entrySet().stream().forEach(e -> execTak.offer(new int[] { e.getValue(), 0 }));
+
+        Queue<int[]> waitingQ = new LinkedList<>();
 
         int cpuTime = 0;
 
-        while(!execTak.isEmpty() || !waitingQ.isEmpty()) {
+        while (!execTak.isEmpty() || !waitingQ.isEmpty()) {
 
-            if(!execTak.isEmpty()) {
-            	cpuTime++;
-            	Task taskWithHighFreq = execTak.poll();
-                taskWithHighFreq.frq--;
-                if(taskWithHighFreq.frq > 0) {
-                    taskWithHighFreq.nextExecutionTime = cpuTime + n;
+            if (!execTak.isEmpty()) {
+                cpuTime++;
+                int[] taskWithHighFreq = execTak.poll();
+                taskWithHighFreq[0]--;
+                if (taskWithHighFreq[0] > 0) {
+                    taskWithHighFreq[1] = cpuTime + n;
                     waitingQ.offer(taskWithHighFreq);
                 }
             }
 
-           if(!waitingQ.isEmpty() && waitingQ.peek().nextExecutionTime == cpuTime) {
+            if (!waitingQ.isEmpty() && waitingQ.peek()[1] == cpuTime) {
                 execTak.offer(waitingQ.poll());
             }
-            
-            
-            if(execTak.isEmpty() && !waitingQ.isEmpty()) { 
-            	cpuTime +=  waitingQ.peek().nextExecutionTime - cpuTime;
-            	execTak.offer(waitingQ.poll());
+
+            if (execTak.isEmpty() && !waitingQ.isEmpty()) {
+                cpuTime += waitingQ.peek()[1] - cpuTime;
+                execTak.offer(waitingQ.poll());
             }
         }
         return cpuTime;
