ef4898b0fc24499163a202a70fbb908f8069ef95
Deepak Kumar <2871622+dpkmcaanna@users.noreply.github.com>
2024-09-15 20:51:42 +0530

    Time: 0 ms (100%), Space: 42.3 MB (27.85%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,3 +1,8 @@
+/*
+There are two rotation required.
+1. Rotate the array along diagonal and then
+2. Rotate along the id column 
+*/
 class Solution {
     public void rotate(int[][] m) {
         int n = m.length;
@@ -12,8 +17,6 @@ class Solution {
 
         int c = m[0].length;
         int mid = c/2;
-
-        System.out.println(mid  + ", " + c);
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < mid ; j++) {
