baa58382a14538c6e2009c045be0897f493bb6b1
Deepak Kumar <2871622+dpkmcaanna@users.noreply.github.com>
2024-10-02 19:59:42 +0530

    Time: 24 ms (53.08%), Space: 55.3 MB (39.69%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -5,12 +5,12 @@ class Solution {
         Map<Integer, Integer> prvSum = new HashMap<>();
         prvSum.put(0, -1);
 
-        int sum = 0, start = 0;
+        int sum = 0, len = 0;
         for(int i = 0; i < nums.length; i++) {
             sum = sum + (nums[i] == 0 ? -1 : 1);
             if(prvSum.containsKey(sum)) {
-                start = prvSum.get(sum) + 1;
-                maxLen = Math.max(maxLen, i - start + 1);
+                len = (i - (prvSum.get(sum) + 1) + 1);
+                maxLen = Math.max(maxLen, len);
             } else {
                 prvSum.put(sum, i);
             }
