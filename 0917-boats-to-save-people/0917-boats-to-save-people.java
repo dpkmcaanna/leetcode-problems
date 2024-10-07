7440b8c81333b820d77de45d95aeb109431dc59b
Deepak Kumar <2871622+dpkmcaanna@users.noreply.github.com>
2024-10-07 21:53:31 +0530

    Time: 16 ms (95.51%), Space: 54.9 MB (54.51%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.java
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -1,7 +1,6 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-
         int left = 0,  right = people.length - 1, botCount = 0;
         while(left <= right) {
             if((left != right) && (people[left] + people[right] <= limit)) {
